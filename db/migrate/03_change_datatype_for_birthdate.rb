f08e02a9ec9876bdd67301302da2ae2491f0b37c
John Hayes <johnnyhayes7@gmail.com>
2019-04-02 16:01:18 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDataTypeForBirthdate < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change
     change_column(table_name, column_name, type) do |s|
